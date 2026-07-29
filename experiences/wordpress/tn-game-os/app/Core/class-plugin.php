bf84263deddd3bcac9c9661a0d95077f6ebd5137
thetngame-maker <thetngame@gmail.com>
2026-07-28 21:19:57 -0500

    Register Explorer test lab

--- experiences/wordpress/tn-game-os/app/Core/class-plugin.php
+++ experiences/wordpress/tn-game-os/app/Core/class-plugin.php
@@ -70,6 +70,7 @@ final class Plugin {
             'app/Modules/Studio/class-tn-studio.php',
             'app/Modules/Admin/class-runtime-audit.php',
             'app/Modules/Admin/class-service-tag-manager.php',
+            'app/Modules/Admin/class-explorer-test-lab.php',
             'app/Modules/Admin/class-admin.php',
         ] as $file) require_once TNG_OS_PATH . $file;
 
@@ -109,6 +110,7 @@ final class Plugin {
             \TNG_OS\Modules\Studio\TN_Studio::class,
             \TNG_OS\Modules\Admin\Runtime_Audit::class,
             \TNG_OS\Modules\Admin\Service_Tag_Manager::class,
+            \TNG_OS\Modules\Admin\Explorer_Test_Lab::class,
             \TNG_OS\Modules\Admin\Admin::class,
         ];
 
