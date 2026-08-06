091db9914d29522bd64760e3ccecbfbe3397766f
thetngame-maker <thetngame@gmail.com>
2026-08-06 06:38:27 -0500

    Add native game detail shell

---
+++ experiences/wordpress/tn-game-os/templates/game-shell.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('ABSPATH')) exit;
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="tng-app" class="tng-router-shell tng-game-shell">
+    <?php echo TNG_Games_UI::detail(); ?>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
