3a44dd01a8ca3a9449cab41a407005ad4ca3d44d
thetngame-maker <thetngame@gmail.com>
2026-08-11 14:25:38 -0500

    Upgrade Explore discovery integration for interactive map

--- experiences/wordpress/tn-game-os/tn-game-explore-discovery-integration.php
+++ experiences/wordpress/tn-game-os/tn-game-explore-discovery-integration.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: TN Game Explore Discovery Integration
  * Description: Connects published TN Game places to the native Explore and Map surfaces.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: The TN Game
  */
 
 if (!defined('ABSPATH')) exit;
 
-define('TNG_EXPLORE_DISCOVERY_VERSION', '0.1.0');
+define('TNG_EXPLORE_DISCOVERY_VERSION', '0.2.0');
 define('TNG_EXPLORE_DISCOVERY_PATH', plugin_dir_path(__FILE__));
 define('TNG_EXPLORE_DISCOVERY_URL', plugin_dir_url(__FILE__));
 
